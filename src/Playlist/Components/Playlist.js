ed0eeaaa773b528749dfbe7cd08490d3db932a49
AGodinez <ing.a.godinez@gmail.com>
2020-07-08 13:26:26 -0500

    now this component is a dump component and render media

--- src/Playlist/Components/Playlist.js
+++ src/Playlist/Components/Playlist.js
@@ -1,20 +1,15 @@
-import React, {Component} from "react";
+import React from "react";
 import Media from "./Media";
+import playlist from "../Css/components/Playlist.css"
 
-class Playlist extends Component{
-    render() {
-        const playlist = this.props.data.categories[0].playlist;
-
-        return (
-            <div>
-                {
-                    playlist.map((item)=>{
-                        return <Media {...item} key={item.id}/>
-                    })
-                }
-            </div>
-        );
-    }
+function Playlist(props) {
+    return (
+        <div className={'playlist-container'}>
+            {
+                <Media {...props} />
+            }
+        </div>
+    );
 }
 
 export default Playlist;
